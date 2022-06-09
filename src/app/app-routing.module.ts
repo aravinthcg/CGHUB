e1bfa4a494d57c912f7a5afdc2939dc5954543ea
Achyuth Gopi krishna <panchala-lila-achyutha.gopi-krishna@capgemini.com>
2022-06-09 13:57:32 +0530

    Added lazy loading components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,55 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/components/home/home.component';
+import { ServiceProviderComponent } from './service-providers/components/service-provider/service-provider.component';
+import { SuperAdminComponent } from './super-admin/components/super-admin/super-admin.component';
+import { UserComponent } from './user/components/user/user.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'Home', component: HomeComponent },
+  {
+    path: 'admin',
+    component: SuperAdminComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./super-admin/super-admin.module').then(
+            (m) => m.SuperAdminModule
+          ),
+      },
+    ],
+  },
+  {
+    path: 'user',
+    component: UserComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./user/user.module').then((m) => m.UserModule),
+      },
+    ],
+  },
+  {
+    path: 'service-providers',
+    component: ServiceProviderComponent,
+    children: [
+      {
+        path: '',
+        loadChildren: () =>
+          import('./service-providers/serivce-provider.module').then(
+            (m) => m.SerivceProviderModule
+          ),
+      },
+    ],
+  },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceProviderComponent } from './service-providers/components/service-provider/service-provider.component';
+import { HomeComponent } from './home/components/home/home.component';
+import { UserComponent } from './user/components/user/user.component';
+import { SuperAdminComponent } from './super-admin/components/super-admin/super-admin.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServiceProviderComponent,
+    HomeComponent,
+    UserComponent,
+    SuperAdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
