a5ef586c02fa281799d0f74c446acc28c563388e
Achyuth Gopi krishna <panchala-lila-achyutha.gopi-krishna@capgemini.com>
2022-06-09 19:10:45 +0530

    Added validations for form

--- src/app/service-providers/components/register-service/register-service.component.ts
+++ src/app/service-providers/components/register-service/register-service.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  Validators,
+  AbstractControl
+} from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-register-service',
@@ -7,17 +13,39 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./register-service.component.scss'],
 })
 export class RegisterServiceComponent implements OnInit {
+  submitted = false;
+  durationInSeconds = 3;
   registerForm = new FormGroup({
     username: new FormControl('', Validators.required),
-    phone: new FormControl(null, Validators.required),
+    phone: new FormControl('', [
+      Validators.required,
+      Validators.minLength(8),
+      Validators.maxLength(10),
+    ]),
     description: new FormControl(''),
-    email: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email]),
   });
-  constructor() {}
+  constructor(private _snackBar: MatSnackBar) {}
 
   ngOnInit(): void {}
 
-  submit(){
-   
+  get f(): { [key: string]: AbstractControl } {
+    return this.registerForm.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+    if (this.registerForm.invalid) {
+      return;
+    } else {
+      this._snackBar.open('Registration success', 'Done', {
+        duration: this.durationInSeconds * 1000,
+      });
+    }
+  }
+
+  onReset(): void {
+    this.submitted = false;
+    this.registerForm.reset();
   }
 }
--- src/app/service-providers/serivce-provider.module.ts
+++ src/app/service-providers/serivce-provider.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
-import {
-  MatRippleModule,
-  MAT_RIPPLE_GLOBAL_OPTIONS,
-} from '@angular/material/core';
 import { RegisterServiceComponent } from './components/register-service/register-service.component';
-import { RouterModule, Routes } from '@angular/router';
+
 import { SerivceProviderRoutingModule } from './service-provider-routing.module';
 import { ServiceProviderComponent } from './components/service-provider/service-provider.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 @NgModule({
   declarations: [RegisterServiceComponent, ServiceProviderComponent],
   imports: [
@@ -19,6 +16,7 @@ import { ReactiveFormsModule } from '@angular/forms';
     MatCardModule,
     MatButtonModule,
     MatFormFieldModule,
+    MatSnackBarModule,
     ReactiveFormsModule,
     SerivceProviderRoutingModule,
   ],
